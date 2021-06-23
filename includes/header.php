db4f57dea39ce654dab41961ebea9f0decfeb16f
Joel July <joeljuly11@gmail.com>
2021-06-24 07:28:47 +0800

    Initial Commit

---
+++ includes/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap</title>
+    <link rel="stylesheet" href="/styles/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  </head>
+  <body>
+    <header>
+      <nav class="navbar-light bg-light">
+        <div class="container-fluid">
+           <div class="row">
+             <div class="col-md-3">
+               <ul class="navbar-nav">
+                 <li class="nav-item">
+                   <a class="nav-link" href="#">Home</a>
+                 </li>
+               </ul>
+
+             </div>
+             <div class="col-md-6 text-center">
+               <a href="#" class="navbar-brand">MONOSCOPE</a>
+             </div>
+             <div class="col-md-3 text-end">
+               <button class="navbar-toggler first-button" type="button" data-bs-toggle="collapse"
+                      data-mdb-target="#navbarToggleExternalContent9" data-bs-target="#navbarToggleExternalContent"
+                      aria-controls="navbarToggleExternalContent9" aria-expanded="false"
+                      aria-label="Toggle navigation">
+                      <div class="animated-icon1"><span></span><span></span><span></span></div>
+                </button>
+
+               <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+               </button> -->
+             </div>
+             <div class="collapse" id="navbarToggleExternalContent">
+               <div class="container-fluid">
+                <nav class="bg-light p-4 navbar-light">
+                  <div class="d-flex justify-content-center">
+                    <div class="nav-item"><a href="#" class="nav-link">About</a></div>
+                    <div class="nav-item"><a href="#" class="nav-link">Login</a></div>
+                    <div class="nav-item"><a href="#" class="nav-link">Features</a></div>
+                    <div class="nav-item"><a href="#" class="nav-link">Contact</a></div>
+                  </div>
+                </nav>
+               </div>
+             </div>
+           </div>
+
+        </div>
+
+      </nav>
+    </header>
