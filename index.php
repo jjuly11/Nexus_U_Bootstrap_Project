d92a58bf0737ff15c1ee494fe3bdbc69bee1b130
Joel July <joeljuly11@gmail.com>
2021-06-25 17:49:31 +0800

    Added Image Gallery ...

--- index.php
+++ index.php
@@ -1,33 +1,94 @@
 <?php
   include_once"includes/header.php";
 ?>
-<section>
+<header class="mb-5">
   <div class="container-fluid p-0">
     <div class="card header-card">
       <img src="./img/erik-cid-RT9e6Di_4ZY-unsplash.jpg" class="card-img h-25" alt="">
       <div class="card-img-overlay row w">
         <div class="row h-100 justify-content-center align-items-center justify-content-center">
           <div class="col-md-12 text-center text-dark">
-            <h5 class="card-title">
-              Welcome to
-            </h5>
-            <h1 class="card-text">
-              Paradise
-            </h1>
+            <div class="bg-white d-inline-block w-50 header-title rounded-top">
+              <h5 class="card-title">
+                Welcome to
+              </h5>
+              <h1 class="card-text">
+                Paradise
+              </h1>
+            </div>
+            
           </div>
         </div>
       </div>
     </div>
   </div>
-</section>
-<section>
+</header>
+
+<!-- Image Gallery -->
+<section class="section1">
   <div class="container">
-    <div class="fs-3 text-center"><u>Featured Images</u></div>
+    <div class="fs-3 text-center mb-3"><u>Featured Images</u></div>
   </div>
   <div class="container-fluid">
-    <div class="d-flex">
+    <div class="d-flex flex-row">
       <div class="card">
         <img src="./img/ahmed-galal-lhzhDD2eEFw-unsplash.jpg" alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./img/chase-baker-QiaaenNiTQU-unsplash.jpg " alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./img/girl-with-red-hat-xPW7_XEZSM4-unsplash.jpg" alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./img/rizky-sabriansyah-HwgZa0tPSeY-unsplash.jpg" alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
+      </div>
+    </div>
+    <!-- Second Row of Images -->
+    <div class="d-flex flex-row">
+      <div class="card">
+        <img src="./img/ahmed-galal-lhzhDD2eEFw-unsplash.jpg" alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./img/chase-baker-QiaaenNiTQU-unsplash.jpg " alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./img/girl-with-red-hat-xPW7_XEZSM4-unsplash.jpg" alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
+      </div>
+      <div class="card">
+        <img src="./img/rizky-sabriansyah-HwgZa0tPSeY-unsplash.jpg" alt="" class="card-img">
+        <div class="card-img-overlay row text-center text-white align-items-end">
+          <div class="card-title fs-3">Image Name</div>
+          <div class="card-text fs-4">Captured by: Author</div>
+        </div>
       </div>
     </div>
   </div>
