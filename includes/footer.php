8143c45bfa3cb23c31a0b6f2d78d9eb7b524b4e0
Joel July <joeljuly11@gmail.com>
2021-06-25 21:36:34 +0800

    Added Explore section with animated button

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,144 @@
+<footer class="bg-light text-center text-dark">
+    <!-- Footer -->
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-linkedin-in"></i
+      ></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+
+
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-dark">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://gitlab.com/jjuly/nexus_u_bootstrap_project">JJuly</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <script src="scripts/main.js" charset="utf-8"></script>
 </body>
--- index.php
+++ index.php
@@ -23,9 +23,17 @@
     </div>
   </div>
 </header>
-
+<section class="mb-5 bg-dark text-white p-5">
+  <div class="container border border-2 text-center p-1">
+    <div class="container text-dark bg-light p-4">
+      <h1 class="mb-4">Come And Explore</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi reiciendis vitae perspiciatis officia esse culpa fuga necessitatibus enim, autem hic, nobis excepturi impedit, accusamus qui distinctio? Quasi, modi, ut consectetur accusantium repellat ea tempore culpa ipsa, tempora saepe quod. Voluptatibus.</p>
+      <button class="btn animate">EXPLORE</button>
+    </div>
+  </div>
+</section>
 <!-- Image Gallery -->
-<section class="section1">
+<section class="section1 mb-3">
   <div class="container">
     <div class="fs-3 text-center mb-3"><u>Featured Images</u></div>
   </div>
@@ -91,6 +99,32 @@
         </div>
       </div>
     </div>
+  </div>
+</section>
+<!-- Image Gallery -->
+
+<section class="section2 p-4 bg-dark">
+  <div class="container">
+    <div class="fs-3 text-center p-3 mb-4 text-white"><u>Subscribe to our news letter</u></div>
+  </div>
+  <div class="container">
+    <form action="">
+      <div class="form-group d-flex flex-row">
+        <input type="email" class="form-control rounded-top" placeholder="Email" id="InputEmail1" aria-describedby="emailHelp">
+        <button type="submit" class="btn bg-primary text-white">Subscribe</button>
+      </div>
+      <div id="emailHelp" class="form-text text-center mt-3">We'll never share your email with anyone else.</div>
+
+      <!-- Section: Text -->
+      <section class="mb-4 text-white mt-5">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+          repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+          eum harum corrupti dicta, aliquam sequi voluptate quas.
+        </p>
+      </section>
+      <!-- Section: Text -->
+    </form>
   </div>
 </section>
 <?php
